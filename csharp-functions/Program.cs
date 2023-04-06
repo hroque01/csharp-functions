6dcd9f32df94c366567ac0da357d3e499710c059
Hadson Roque <nineonezero.light@gmail.com>
2023-04-06 16:39:31 +0200

    Quarto esercizio

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using System.Linq;
 
 namespace csharp_functions
 {
@@ -6,16 +7,35 @@ namespace csharp_functions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("********* PRIMO ESERCIZIO **********");
             //Primo esercizio
             int[] myArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             StampaArray(myArr);
 
+            Console.WriteLine("********* SECONDO ESERCIZIO **********");
             //Secondo esercizio
             Quadrato(5);
             Console.WriteLine(Quadrato(5));
 
+            Console.WriteLine("********* TERZO ESERCIZIO **********");
             //Terzo esercizio
             StampaArray(ElevaArrayAlQuadrato(myArr));
+
+            Console.WriteLine("********* QUARTO ESERCIZIO **********");
+            //Quarto esercizio
+            int[] myArrSum = { 2, 6, 7, 5, 3, 9 };
+
+            Console.WriteLine("Stampare l'array fornito: ");
+            StampaArray(myArrSum);
+
+            Console.WriteLine("Stampare l'array fornito elevato al quadrato: ");
+            StampaArray(ElevaArrayAlQuadrato(myArrSum));
+
+            Console.WriteLine("Stampare la somma dell'array: ");
+            Console.WriteLine(Somma(myArrSum));
+
+            Console.WriteLine(Quadrato(Somma(myArrSum)));
+
         }
 
         //Primo esercizio
@@ -42,6 +62,17 @@ namespace csharp_functions
                 array[i] *= array[i];
             }
             return array;
+        }
+
+        //Quarto Esercizio
+        public static int Somma(int[] Array)
+        {
+            int sum = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                sum += Array[i];
+            }
+            return sum;
         }
     }
 }
