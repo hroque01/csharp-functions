c29d15a7ebed8af2c6cdbb8f5887e2bc3bd96d5e
Hadson Roque <nineonezero.light@gmail.com>
2023-04-06 15:59:19 +0200

    Create quadrato function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
+            //Primo esercizio
             int[] myArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             StampaArray(myArr);
+
+            //Secondo esercizio
+            Quadrato(5);
+            Console.WriteLine(Quadrato(5));
+            
+            //Terzo esercizio
         }
 
+        //Primo esercizio
         public static void StampaArray(int[] array)
         {   
             for (int i = 0; i < array.Length; i++)
@@ -15,5 +23,14 @@
                 Console.WriteLine($"Elemento {i}");
             }
         }
+
+        //Secondo esercizio
+        public static int Quadrato(int numero)
+        {
+            int potenza = numero * numero;
+            return potenza;
+        }
+
+       
     }
 }
