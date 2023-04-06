baadfe63501292bfe7967677a9edcbedc9dcede5
Hadson Roque <nineonezero.light@gmail.com>
2023-04-06 15:40:50 +0200

    Add project files.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace csharp_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            StampaArray(myArr);
+        }
+
+        public static void StampaArray(int[] array)
+        {   
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Elemento {i}");
+            }
+        }
+    }
+}
